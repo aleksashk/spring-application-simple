2369bb8bc11545f2ea702d2313f6d770d6d220a2
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-16 17:56:47 +0300

    lesson 3 DI with setter

--- src/main/java/ru/philimonov/springcourse/MusicPlayer.java
+++ src/main/java/ru/philimonov/springcourse/MusicPlayer.java
@@ -3,8 +3,15 @@ package ru.philimonov.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    public MusicPlayer() {
+    }
+
     //IoC
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
