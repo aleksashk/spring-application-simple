a9f715069c6ef03045ae6cb6ce0907602e00d32e
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-16 17:46:09 +0300

    lesson 2 Inversion of Control (IoC) part second

--- src/main/java/ru/philimonov/springcourse/TestSpring.java
+++ src/main/java/ru/philimonov/springcourse/TestSpring.java
@@ -5,8 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = ctx.getBean("musicBean", Music.class);
-        MusicPlayer player = new MusicPlayer(music);
+//        Music music = ctx.getBean("musicBean", Music.class);
+//        MusicPlayer player = new MusicPlayer(music);
+
+        MusicPlayer player = ctx.getBean("musicPlayer", MusicPlayer.class);
         player.playMusic();
         ctx.close();
     }
