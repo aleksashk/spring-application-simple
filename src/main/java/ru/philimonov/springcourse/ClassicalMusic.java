cf97ebc46cd08d5693cc20a59217b41ab4b9e6a4
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-17 16:01:00 +0300

    lesson 9 DI @PostConstruct, @PreDestroy

--- src/main/java/ru/philimonov/springcourse/ClassicalMusic.java
+++ src/main/java/ru/philimonov/springcourse/ClassicalMusic.java
@@ -3,9 +3,22 @@ package ru.philimonov.springcourse;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
+
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doInit(){
+        System.out.println("doing initialization!!!");
+    }
+
+    @PreDestroy
+    public void doDestroy(){
+        System.out.println("doing destruction!!!");
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
