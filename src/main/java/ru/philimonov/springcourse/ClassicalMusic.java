b6071fffe828f7d5bd3cc4f037d9612cda450e15
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-17 13:10:36 +0300

    lesson 5 factory-method

--- src/main/java/ru/philimonov/springcourse/ClassicalMusic.java
+++ src/main/java/ru/philimonov/springcourse/ClassicalMusic.java
@@ -1,16 +1,23 @@
 package ru.philimonov.springcourse;
 
-public class ClassicalMusic implements Music{
+public class ClassicalMusic implements Music {
+    private ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
     @Override
     public String getSong() {
         return "Hungarian rhapsody";
     }
 
-    public void doMyInit(){
+    public void doMyInit() {
         System.out.println("doing initialization!!!");
     }
 
-    public void doMyDestroy()
-    {
+    public void doMyDestroy() {
         System.out.println("doing destruction!!!");
-    }}
+    }
+}
